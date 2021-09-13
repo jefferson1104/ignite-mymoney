d15a2cef1fed1c1e2fc4be7ba8ebe0bb05b4b895
jefferson1104 <jefferson1104junior@gmail.com>
2021-09-13 19:46:57 -0300

    salvando e consumindo dados da api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
     api.get('https://localhost:3000/api/transactions')
-      .then(response => console.log(response.data))
+      .then(response => setTransactions(response.data.transactions))
   }, []);
 
   return (
@@ -21,36 +32,23 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Trabalho</td>
-            <td>15/08/2021</td>
-          </tr>
-          <tr>
-            <td>Conta de luz</td>
-            <td className="withdraw">-R$150</td>
-            <td>Casa</td>
-            <td>20/08/2021</td>
-          </tr>
-          <tr>
-            <td>Conta de água</td>
-            <td className="withdraw">-R$80</td>
-            <td>Casa</td>
-            <td>25/08/2021</td>
-          </tr>
-          <tr>
-            <td>Rodizio boi nobre</td>
-            <td className="withdraw">-R$220</td>
-            <td>Restaurante</td>
-            <td>27/08/2021</td>
-          </tr>
-          <tr>
-            <td>Parque aquático</td>
-            <td className="withdraw">-R$100</td>
-            <td>Lazer</td>
-            <td>27/08/2021</td>
-          </tr>
+        {transactions.map(transaction => {
+          return (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR', ).format(new Date(transaction.createdAt))}
+              </td>
+            </tr>
+          )
+        })}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance website',
+          type: 'deposit',
+          category: 'Trabalho',
+          amount: 6000,
+          createdAt: new Date('2021-08-30 14:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Salário',
+          type: 'deposit',
+          category: 'Trabalho',
+          amount: 11000,
+          createdAt: new Date('2021-08-15 09:00:00'),
+        },
+        {
+          id: 3,
+          title: 'Alugel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1500,
+          createdAt: new Date('2021-08-20 10:00:00'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 350,
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data =  JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
   }
 });
 
